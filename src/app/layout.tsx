b11c243112add81dcf2831568cac9af57e933c90
Hasegawa Yuta <gssbtsm999@gmail.com>
2025-07-22 21:48:51 +0900

    revert

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,4 @@
 import "./globals.css";
-import { createMeta } from "@/create-meta";
-
-export const metadata = createMeta({
-  title: "Home title",
-  description: "Home description",
-  openGraph: {
-    type: "website",
-    title: "Home openGraph.title",
-    description: "Home openGraph.description",
-  },
-});
 
 export default function RootLayout({
   children,
